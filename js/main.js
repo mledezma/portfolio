37cc80cba0376e371759c6c52e9ebe7bdffd2e2d
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-10-23 23:23:11 -0600

    Added cool functionality to the textarea of the form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('Hello')
+(function(window){
+    var init = function() {
+        var textareaArray = document.querySelectorAll("textarea");
+        var heightLimit = 200; /* Maximum height: 200px */
+        
+        textareaArray.forEach(function(textarea) {
+
+            textarea.oninput = function() {
+                console.log('waaat')
+                textarea.style.height = ""; /* Reset the height*/
+                textarea.style.height = Math.min(textarea.scrollHeight, heightLimit) + "px";
+            };
+        })
+        
+    }
+
+    window.app = {
+        init: init
+    }
+
+}(window))
+
+app.init();
